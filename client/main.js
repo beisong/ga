9fd9673e9db972cc16ba91da7ccd05fbaea3e5b9
weisong <weisong@renewgpl.com>
2016-11-13 10:33:32 +0800

    added test button w test api

--- client/main.js
+++ client/main.js
@@ -3,3 +3,15 @@ Meteor.subscribe('movie_list');
 Template.main.helpers({
     movies: Movies.find({})
 });
+
+Template.main.events({
+    'click #test_button_id': function (e) {
+        e.preventDefault();
+        console.log("You pressed the button");
+        Meteor.call("callapi", "abc", function (err, result) {
+            console.log(result);
+            console.log(result.content);
+        });
+
+    }
+});
--- server/methods.js
+++ server/methods.js
@@ -2,4 +2,9 @@ Meteor.methods({
     // addReason: function (code, description) {
     //     Reason_options.insert({code: code, description: description});
     // }
+    callapi: function (input) {
+        this.unblock();
+        return Meteor.http.call("GET", "http://jsonplaceholder.typicode.com/posts/1");
+        //call skyscanner api with api key
+    }
 });
